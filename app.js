d10078c59d72f3db1e12bfd8521612b8d0a6ea73
aangupta <aanchalgupta457@gmail.com>
2021-02-21 21:30:22 +0530

    exploring express.static

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/static', express.static('Public'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
